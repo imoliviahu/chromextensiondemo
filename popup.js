f981ba46f8f9e62782a5994b877f1689b1bed185
imoliviahu <huo282@newschool.edu>
2023-12-09 21:22:15 -0500

    added chrome extension

--- popup.js
+++ popup.js
@@ -1,6 +1,1 @@
-let clickedEl = document.getElementById('clickedTimes');
-let clickedCount = 0;
-chrome.storage.local.get(["clickedTimes"]).then((result) => {
-    clickedCount = result.clickedTimes || 0;
-    clickedEl.innerText = `Clicked ${clickedCount}`;
-});
+console.log("This is a popup!");
