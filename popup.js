30ce58a7a43ea13f8c9e1524f77ce8a7b6919128
imoliviahu <huo282@newschool.edu>
2023-12-10 19:10:13 -0500

    updates

--- popup.js
+++ popup.js
@@ -1,1 +1,31 @@
 console.log("This is a popup!");
+
+function setReminder() {
+  // Get the HTML element to display the reminder
+  const reminderDisplay = document.getElementById("reminderDisplay");
+
+  // Set a reminder function
+  function remind() {
+    // Display the reminder in the HTML element
+    reminderDisplay.innerHTML =
+      "Time to take a break: Drink some water, rest your eyes, stretch, and get some food if your hungry!";
+    // You can replace this line with your own reminder logic
+  }
+
+  // Set a reminder every 20 seconds
+  const interval = 10 * 1000;
+
+  // Set the initial reminder
+  setTimeout(function repeatReminder() {
+    remind();
+    // Set the next reminder
+    setTimeout(function () {
+      // Clear the reminder text after a short delay (e.g., 2 seconds)
+      reminderDisplay.innerHTML = "";
+      repeatReminder();
+    }, 2000); // 2000 milliseconds = 2 seconds
+  }, interval);
+}
+
+// Call the function to start the reminders
+setReminder();
